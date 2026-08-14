e13d15ad6cdec42ebfeaa6bb1a79d3a8beb8bc50
Diego Cano <57439165+dfc-coder@users.noreply.github.com>
2026-08-14 15:35:02 -0300

    refactor: export reranker port

--- src/rag_ops_guard/ports/__init__.py
+++ src/rag_ops_guard/ports/__init__.py
@@ -1,3 +1,9 @@
-from rag_ops_guard.ports.interfaces import ChatModel, EmbeddingProvider, ObjectStore, VectorStore
+from rag_ops_guard.ports.interfaces import (
+    ChatModel,
+    EmbeddingProvider,
+    ObjectStore,
+    Reranker,
+    VectorStore,
+)
 
-__all__ = ["ChatModel", "EmbeddingProvider", "ObjectStore", "VectorStore"]
+__all__ = ["ChatModel", "EmbeddingProvider", "ObjectStore", "Reranker", "VectorStore"]
